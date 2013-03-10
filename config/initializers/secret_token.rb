f8ee78c5f5b3a7c03ef36f4875b64cf2996903b0
Martin Dean <mizpah@enigmaguild.org>
2013-03-10 22:13:59 +0000

    Base Commited

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = '63dc4ece852528f9cec0194be8e9b9db84a80a30943188464e6f3650088a8533b162a5c67fb6f46efa79453484ceff0ff4921afbd4c5db805b31102f50bf3014'
