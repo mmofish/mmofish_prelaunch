596ba97cda0abee5325cde94e2be027346738105
Martin Dean <mizpah@enigmaguild.org>
2013-03-11 02:53:22 +0000

    Changed Factory Girl user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,8 @@
 
 FactoryGirl.define do
   factory :user do
-    name 'Test User'
-    email 'example@example.com'
+    name 'Test'
+    email 'test@mmofish.com'
     password 'changeme'
     password_confirmation 'changeme'
     # required if the Devise Confirmable module is used
