1943abe18395082f37b6d2191d88d3162aa85ce1
Martin Dean <mizpah@enigmaguild.org>
2013-03-12 00:01:09 +0000

    Added Enviroment VAR got Mailchip into Devise user signup initializer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
-  def index
-    @users = User.all
-  end
+  
+  #removed index to test
+  #def index
+  #  @users = User.all
+  #end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -262,4 +262,11 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = "/my_engine/users/auth"
+  
+  # Adding mailchimp api and email list for integration into 
+  # devise user signup form
+  
+  Devise.mailchimp_api_key = ENV['MAILCHIMP_API_KEY']
+  Devise.mailing_list_name = ENV['MAILING_LIST_NAME']
+  
 end
