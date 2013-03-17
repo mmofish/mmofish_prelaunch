c3600bedd94f17e7d74a31fffb505890ca5785d4
Martin Dean <mizpah@enigmaguild.org>
2013-03-17 18:52:20 +0000

    Dashboard Icons and Menu Structure in Admin

--- config/admin_navigation.rb
+++ config/admin_navigation.rb
@@ -1,7 +1,10 @@
 SimpleNavigation::Configuration.run do |navigation|
 
   navigation.items do |primary|
-    primary.item :key_1, 'Dashboard', admin_path do |sub_nav|
+    primary.dom_id = 'admin_master_menu nav nav-list'
+    primary.dom_class = 'nav nav-list'
+    primary.item :key_1, '<i class="icon-dashboard icon-2x si"></i>', admin_path, {:id => 'dashboard_nav'} do |sub_nav|
+        sub_nav.dom_class = 'nav inline'
         sub_nav.item :key_1_1, 'A', nil
         sub_nav.item :key_1_2, 'B', nil
         sub_nav.item :key_1_3, 'C', nil
@@ -9,7 +12,8 @@ SimpleNavigation::Configuration.run do |navigation|
         sub_nav.item :key_1_5, 'E', nil 
     end
     
-    primary.item :key_2, 'Users &  Content', admin_users_path do |sub_nav|
+    primary.item :key_2, '<i class="icon-group icon-2x si"></i>', admin_users_path, {:id => 'users_nav'} do |sub_nav|
+        sub_nav.dom_class = 'nav inline'
         sub_nav.item :key_2_1, 'Users', admin_users_path
         sub_nav.item :key_2_2, 'Green', nil
         sub_nav.item :key_2_3, 'Yellow', nil
@@ -17,7 +21,8 @@ SimpleNavigation::Configuration.run do |navigation|
         sub_nav.item :key_2_5, 'Orange', nil
     end
     
-    primary.item :key_3, 'Settings', nil do |sub_nav|
+    primary.item :key_3, '<i class="icon-check icon-2x si"></i>', nil, {:id => 'key3_nav'} do |sub_nav|
+        sub_nav.dom_class = 'nav inline'
         sub_nav.item :key_3_1, 'Warrior', nil
         sub_nav.item :key_3_2, 'Priest', nil
         sub_nav.item :key_3_3, 'Tank', nil
@@ -25,7 +30,8 @@ SimpleNavigation::Configuration.run do |navigation|
         sub_nav.item :key_3_5, 'Bard', nil
     end
     
-    primary.item :key_4, 'Visuals', nil do |sub_nav|
+    primary.item :key_4, '<i class="icon-pencil icon-2x si"></i>', nil, {:id => 'key4_nav'} do |sub_nav|
+        sub_nav.dom_class = 'nav inline'
         sub_nav.item :key_4_1, 'Square', nil
         sub_nav.item :key_4_2, 'Circle', nil
         sub_nav.item :key_4_3, 'Rectangle', nil
@@ -34,7 +40,8 @@ SimpleNavigation::Configuration.run do |navigation|
     
     end
     
-    primary.item :key_5, 'Configuration', nil do |sub_nav|
+    primary.item :key_5, '<i class="icon-cogs icon-2x si"></i>', nil, {:id => 'key5_nav'} do |sub_nav|
+        sub_nav.dom_class = 'nav inline'
         sub_nav.item :key_5_1, 'A Setting', nil
         sub_nav.item :key_5_2, 'A Slider', nil
         sub_nav.item :key_5_3, 'A Control', nil
